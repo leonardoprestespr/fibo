2a8483e46a8217c8b902b6c5238d4c69bc8d7fb6
Leonardo Prestes <contatollprestes@gmail.com>
2024-09-08 21:39:20 -0300

    testes

--- projeto-02/verificador.py
+++ projeto-02/verificador.py
@@ -4,15 +4,18 @@
 
 def verifica_a(string):
     string_lower = string.lower()
+#verifica a exitência da letra 'a'
     if 'a' in string_lower:
         print("..........................")
         print("A letra 'a' foi encontrada.")
     else:
         print("...........................")
         print("A letra 'a' não foi encontrada")
+#conta a quantida de vezes que aparece
     qtd_a = string_lower.count('a')
     print('.................................')
     print(f"A letra 'a' aparece {qtd_a} vez(es) na frase")
+#entrada usuário
 string = input("Digite uma frase: ")
 
 verifica_a(string)
