481f606d9afc4c9f14d6dd6628b52ac8fb7979e9
Leonardo Prestes <contatollprestes@gmail.com>
2024-09-08 21:16:12 -0300

    testes

--- projeto-02/verificador.py
+++ projeto-02/verificador.py
@@ -1,3 +1,18 @@
 # Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
 
 #IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+def verifica_a(string):
+    string_lower = string.lower()
+    if 'a' in string_lower:
+        print("..........................")
+        print("A letra 'a' foi encontrada.")
+    else:
+        print("...........................")
+        print("A letra 'a' não foi encontrada")
+    qtd_a = string_lower.count('a')
+    print('.................................')
+    print(f"A letra 'a' aparece {qtd_a} vez(es) na frase")
+string = input("Digite uma frase: ")
+
+verifica_a(string)
